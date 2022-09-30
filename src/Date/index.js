8524b7dd0361836255533654fafbf781657f5609
Szymon Kedra <s5zee@wp.pl>
2022-09-30 16:26:11 +0200

    extracted date formatting code out of jsx

--- src/Date/index.js
+++ src/Date/index.js
@@ -2,24 +2,24 @@ import "./style.css";
 import { useState, useEffect } from "react";
 
 const DateTime = () => {
- const [dateTime, setDateTime] = useState(new Date());
+  const [dateTime, setDateTime] = useState(new Date());
+  const timeString = dateTime.toLocaleTimeString()
+  const dateString = dateTime.toLocaleDateString(undefined,
+    { weekday: "long", day: "numeric", month: "long" });
 
- useEffect(() => {
-  const intervalID = setInterval(() =>
-   setDateTime(new Date()), 1000);
-  return () => (
-   clearInterval(intervalID)
+  useEffect(() => {
+    const intervalID = setInterval(() =>
+      setDateTime(new Date()), 1000);
+    return () => (
+      clearInterval(intervalID)
+    )
+  }, []);
+
+  return (
+    <span className="fullDate">
+      Dzisiaj jest {dateString}, {timeString}
+    </span>
   )
- }, []);
-
- return (
-  <span className="fullDate">
-   Dzisiaj jest {`${dateTime.toLocaleDateString
-    ("pl",
-     { weekday: "long", day: "numeric", month: "long" })},
-      ${dateTime.toLocaleTimeString()}`}
-  </span>
- )
 }
 export default DateTime;
 
