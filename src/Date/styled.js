87f2930a5e1b312e7f8330c0a803bda20e89c792
Szymon Kedra <s5zee@wp.pl>
2022-10-04 18:57:11 +0200

    removed component that is to be renamed

--- src/Date/styled.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const Span = styled.span`
-    display: flex;
-    justify-content: flex-end;
-    font-family: monospace;
-    font-size: 12px;
-    color: grey;
-`;
