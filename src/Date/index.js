16707f759451b44f83b3421cf2215766bc01df4f
Szymon Kedra <s5zee@wp.pl>
2022-10-04 16:13:27 +0200

    custom hook useCurrentDate added

--- src/Date/index.js
+++ src/Date/index.js
@@ -1,23 +1,12 @@
-import { useState, useEffect } from "react";
 import { Span } from "./styled"
+import { useCurrentDate } from "./useCurrentDate";
 
 const DateTime = () => {
-  const [dateTime, setDateTime] = useState(new Date());
-  const timeString = dateTime.toLocaleTimeString()
-  const dateString = dateTime.toLocaleDateString(undefined,
-    { weekday: "long", day: "numeric", month: "long" });
-
-  useEffect(() => {
-    const intervalID = setInterval(() =>
-      setDateTime(new Date()), 1000);
-    return () => (
-      clearInterval(intervalID)
-    )
-  }, []);
+  const date = useCurrentDate()
 
   return (
     <Span>
-      Dzisiaj jest {dateString}, {timeString}
+      Dzisiaj jest {date}
     </Span>
   )
 }
