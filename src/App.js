6558aebc1e88549b4b5e997d0fe6bf7e07d4b878
Szymon Kedra <s5zee@wp.pl>
2022-10-20 19:39:11 +0200

    moved logic from App to Form

--- src/App.js
+++ src/App.js
@@ -1,44 +1,14 @@
 import Form from "./Form";
 import Container from "./Container";
-import { useState } from "react";
-import Result from "./Result";
 import Clock from "./Clock";
 
 
 function App() {
-  const [resultElement, setResultElement] = useState([
-    { id: 1, content: "N/A" },
-  ]);
-
-  const calculateResult =
-    (amountValue, initialCurrency, secondaryCurrency) => {
-      if (initialCurrency === "") {
-        return;
-      } else if (secondaryCurrency === "") {
-        return;
-      }
-      setResultElement(resultElement.map(result => {
-        return {
-          ...result, content: amountValue *
-            initialCurrency / secondaryCurrency
-        }
-      }));
-    };
-
   return (
     <Container
       date={<Clock />}
-      body={
-        <Form
-          calculateResult={calculateResult}
-        />
-      }
-      extraContent={
-        <Result
-          resultElement={resultElement}
-        />
-      }
+      body={<Form />}
     />
   );
-}
+};
 export default App;
