aaa1f622538b882c25ca893450970447243ff3dd
Szymon Kedra <s5zee@wp.pl>
2022-10-23 00:47:33 +0200

    destructured result, view based on template string

--- src/Result/index.js
+++ src/Result/index.js
@@ -1,13 +1,24 @@
 import { Container } from "./styled";
 
-const Result = ({ result }) => (
-  <Container>
-    {result.InputAmount} {""}
-    {result.initialCurrency} = {""}
-    {typeof result.calculatedAmount === "number" ?
-      result.calculatedAmount.toFixed(2) :
-      result.calculatedAmount} {""}
-    {result.secondaryCurrency}
-  </Container>
-);
+const Result = ({ result }) => {
+  const { inputAmount, initialCurrency,
+    calculatedAmount, secondaryCurrency } = result;
+
+  const roundedCalculatedAmount =
+    typeof calculatedAmount === "number" ?
+      calculatedAmount.toFixed(2) :
+      calculatedAmount;
+
+  const formattedResult = result
+  && `${inputAmount} 
+  ${initialCurrency} = 
+  ${roundedCalculatedAmount}
+  ${secondaryCurrency}`;
+
+  return (
+    <Container>
+      {formattedResult}
+    </Container>
+  );
+};
 export default Result;
