71b221b2d17a30e208e1bde41d5cf2a10e95647c
Szymon Kedra <s5zee@wp.pl>
2022-10-23 00:44:10 +0200

    put 3 similar states variable and placed them in 1

--- src/Form/useApiData.js
+++ src/Form/useApiData.js
@@ -2,24 +2,25 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 export const useApiData = () => {
-    const [ratesData, setRatesData] = useState({ rates: "EUR" });
-    const [date, setDate] = useState({ date: "" });
-    const [state, setState] = useState({ state: "loading" })
+    const [ratesData, setRatesData] = useState({
+        rates: null, date: "", state: "loading"
+    });
 
     useEffect(() => {
         const apiData = async () => {
             try {
-                const response = await axios.get
-                    ("https://api.exchangerate.host/latest?base=PLN")
-                setState({ state: "success" })
-                setRatesData({ rates: (response.data.rates) });
-                setDate({ date: (response.data.date) });
+                const response = await axios.get("https://api.exchangerate.host/latest?base=PLN");
+                setRatesData({
+                    rates: (response.data.rates),
+                    date: (response.data.date),
+                    state: "success"
+                });
             } catch (error) {
-                setState({ state: "error" })
-            }
+                setRatesData({ state: "error" });
+            };
 
         }; setTimeout(apiData, 1000);
-    }, [])
+    }, []);
 
-    return { state, date, ratesData };
+    return { ratesData };
 };
