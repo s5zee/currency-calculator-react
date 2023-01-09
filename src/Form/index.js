fb802a82fac619b4564c86986a02dea55fb4cf4f
Szymon Kedra <s5zee@wp.pl>
2023-01-09 20:42:08 +0100

    formatting the code

--- src/Form/index.js
+++ src/Form/index.js
@@ -17,71 +17,83 @@ const Form = () => {
     calculateResult(amountValue, initialCurrency, secondaryCurrency, ratesData);
   };
 
-  const calculateResult =
-    (amountValue, initialCurrency, secondaryCurrency, ratesData) => {
-      const initialSelectRate = ratesData.rates[initialCurrency];
-      const secondarySelectRate = ratesData.rates[secondaryCurrency];
-      setResult({
-        inputAmount: amountValue,
-        initialCurrency,
-        calculatedAmount: amountValue * initialSelectRate / secondarySelectRate,
-        secondaryCurrency
-      });
-    };
+  const calculateResult = (
+    amountValue,
+    initialCurrency,
+    secondaryCurrency,
+    ratesData
+  ) => {
+    const initialSelectRate = ratesData.rates[initialCurrency];
+    const secondarySelectRate = ratesData.rates[secondaryCurrency];
+    setResult({
+      inputAmount: amountValue,
+      initialCurrency,
+      calculatedAmount: (amountValue * initialSelectRate) / secondarySelectRate,
+      secondaryCurrency,
+    });
+  };
 
   return (
     <form onSubmit={onFormSubmit}>
       <Header>Kalkulator walutowy</Header>
       {ratesData.state === "loading" ? (
-        <Span>Trwa ładowanie proszę czekać.</Span>) :
-        (ratesData.state === "error") ?
-          (<Span>Uwaga wystąpił błąd,
-            sprawdź swoje połączenie internetowe
-            lub spróbuj ponownie za chwile.</Span>) :
-          ratesData.state === "success" ? (
-            <>
-              <Paragraph>
-                <Label title="Mam:" />
-                <Field as="select"
-                  value={initialCurrency}
-                  onChange={({ target }) => setinitialCurrency(target.value)}
-                >
-                  {Object.keys(ratesData.rates).map((rate) => (
-                    <option key={rate} value={rate}>{rate}</option>
-                  ))}
-                </Field>
-              </Paragraph>
-              <Paragraph>
-                <Label title="Kwota do przeliczenia:" />
-                <Field
-                  value={amountValue}
-                  required type="number"
-                  min="0.01"
-                  step="0.01"
-                  onChange={({ target }) => setAmountValue(target.value)}
-                />
-              </Paragraph>
-              <Paragraph>
-                <Label title="Chcę otrzymać" />
-                <Field as="select"
-                  value={secondaryCurrency}
-                  onChange={({ target }) => setSecondaryCurrency(target.value)}
-                >
-                  {Object.keys(ratesData.rates).map(rate => (
-                    <option key={rate} value={rate}>{rate}</option>
-                  ))}
-                </Field>
-              </Paragraph>
-              <Button title={"Oblicz wartość"} />
-              <Container>
-                Kursy walut pobierane są z ECB (European central bank)
-              </Container>
-              <Container>aktualne na dzień: <b>{ratesData.date}</b> </Container>
-              <Result result={result} />
-            </>
-          )
-            : null
-      }
+        <Span>Trwa ładowanie proszę czekać.</Span>
+      ) : ratesData.state === "error" ? (
+        <Span>
+          Uwaga wystąpił błąd, sprawdź swoje połączenie internetowe lub spróbuj
+          ponownie za chwile.
+        </Span>
+      ) : ratesData.state === "success" ? (
+        <>
+          <Paragraph>
+            <Label title="Mam:" />
+            <Field
+              as="select"
+              value={initialCurrency}
+              onChange={({ target }) => setinitialCurrency(target.value)}
+            >
+              {Object.keys(ratesData.rates).map((rate) => (
+                <option key={rate} value={rate}>
+                  {rate}
+                </option>
+              ))}
+            </Field>
+          </Paragraph>
+          <Paragraph>
+            <Label title="Kwota do przeliczenia:" />
+            <Field
+              value={amountValue}
+              required
+              type="number"
+              min="0.01"
+              step="0.01"
+              onChange={({ target }) => setAmountValue(target.value)}
+            />
+          </Paragraph>
+          <Paragraph>
+            <Label title="Chcę otrzymać" />
+            <Field
+              as="select"
+              value={secondaryCurrency}
+              onChange={({ target }) => setSecondaryCurrency(target.value)}
+            >
+              {Object.keys(ratesData.rates).map((rate) => (
+                <option key={rate} value={rate}>
+                  {rate}
+                </option>
+              ))}
+            </Field>
+          </Paragraph>
+          <Button title={"Oblicz wartość"} />
+          <Container>
+            Kursy walut pobierane są z ECB (European central bank)
+          </Container>
+          <Container>
+            aktualne na dzień: <b>{ratesData.date}</b>{" "}
+          </Container>
+          <Result result={result} />
+        </>
+      ) : null}
     </form>
   );
 };
--- src/Form/useApiData.js
+++ src/Form/useApiData.js
@@ -2,25 +2,29 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 export const useApiData = () => {
-    const [ratesData, setRatesData] = useState({
-        rates: null, date: "", state: "loading"
-    });
+  const [ratesData, setRatesData] = useState({
+    rates: null,
+    date: "",
+    state: "loading",
+  });
 
-    useEffect(() => {
-        const apiData = async () => {
-            try {
-                const response = await axios.get("https://api.exchangerate.host/latest?base=PLN");
-                setRatesData({
-                    rates: (response.data.rates),
-                    date: (response.data.date),
-                    state: "success"
-                });
-            } catch (error) {
-                setRatesData({ state: "error" });
-            };
+  useEffect(() => {
+    const apiData = async () => {
+      try {
+        const response = await axios.get(
+          "https://api.exchangerate.host/latest?base=PLN"
+        );
+        setRatesData({
+          rates: response.data.rates,
+          date: response.data.date,
+          state: "success",
+        });
+      } catch (error) {
+        setRatesData({ state: "error" });
+      }
+    };
+    setTimeout(apiData, 1000);
+  }, []);
 
-        }; setTimeout(apiData, 1000);
-    }, []);
-
-    return { ratesData };
+  return { ratesData };
 };
